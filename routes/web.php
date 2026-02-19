f9691bd76ecdffed70d72c221dc2f8a7fe643f1b
Daniel Enrique Villamizar <dvillamizar435@unab.edu.co>
2026-02-19 11:07:46 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/product', [ProductController::class,'index']);
+Route::prefix('product')->controller(ProductController::class)->group(function (){
+    Route::get('/product', 'index');
+    Route::get('/create','create');
+    Route::get('/{producto}','show');
+    });
 
-Route::get('/product/create',[ProductController::class,'create']);
 
-Route::get('/product/{producto}',[ProductController::class,'show']);
